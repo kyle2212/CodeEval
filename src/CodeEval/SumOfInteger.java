84510cef91df46f5f09a62f0cc2c3d246ee0f80f
Kyle Fong <kfong@guidewire.com>
2016-02-02 17:35:25 -0500

    SumOfInteger.

---
+++ src/CodeEval/SumOfInteger.java
@@ -0,0 +1,17 @@
+package CodeEval;
+
+import java.io.*;
+
+public class SumOfInteger {
+  public static void main(String[] args) throws IOException {
+    File file = new File(args[0]);
+    BufferedReader buffer = new BufferedReader(new FileReader(file));
+    String line;
+    int sum = 0;
+    while ((line = buffer.readLine()) != null) {
+      line = line.trim();
+      sum += Integer.parseInt(line);
+    }
+    System.out.println(sum);
+  }
+}
